43919c173a1714086f7c5cf4f85aae3fd86a4ed6
Vitalii Goncharov <1c.audit@gmail.com>
2025-03-16 10:49:05 +0200

    add turbosms sample

--- routes/console.php
+++ routes/console.php
@@ -3,8 +3,17 @@
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Notifications\Sms;
 
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command(
+    'send-sms {route}?} {body}?}',
+    function ($route, $body) {
+        Notification::route('turbosms', $route)
+            ->notify(new Sms($body));
+    }
+)->describe('send test SMS');
